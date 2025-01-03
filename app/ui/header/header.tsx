2aea77aadd7ebdc60ba3a84607e1f37be6632da6
radekBednarik <radek.bednarik@stesena.com>
2025-01-03 09:44:38 +0100

    header formatting

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Header() {
 	return (
-		<header id="header">
-			<div className="inline-flex flex-col">
+		<header id="header" className="flex justify-end h-screen">
+			<div className="inline-flex flex-col ">
 				<h1 className="text-8xl">Weather Forecast</h1>
 				<h2 className="text-5xl self-end">for your location</h2>
 			</div>
