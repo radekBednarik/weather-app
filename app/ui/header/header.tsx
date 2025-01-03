96ec39e613ba5935dd825c4217b069a7bb624f7a
radekBednarik <radek.bednarik@stesena.com>
2025-01-03 09:24:56 +0100

    header added

---
+++ app/ui/header/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+	return (
+		<header id="header">
+			<div className="inline-flex flex-col">
+				<h1 className="text-8xl">Weather Forecast</h1>
+				<h2 className="text-5xl self-end">for your location</h2>
+			</div>
+		</header>
+	);
+}
