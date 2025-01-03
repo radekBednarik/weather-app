a46bd23697b4cad82b50b19212f479166b8a9d25
radekBednarik <radek.bednarik@stesena.com>
2025-01-03 08:34:05 +0100

    add api call function

---
+++ app/lib/met-api/api.ts
@@ -0,0 +1,57 @@
+/**
+ * This module handles calling METNO API which return
+ * JSON formatted data about weather forecast.
+ * @see https://api.met.no/weatherapi/locationforecast/2.0/documentation
+ */
+
+import type { MetJsonForecast } from "@/app/lib/met-api/declarations";
+
+export interface ApiError {
+	message?: string;
+	status_code?: number;
+	error?: Error;
+}
+
+export async function getForecastData({
+	latitude,
+	longitude,
+	altitude,
+}: { latitude: number; longitude: number; altitude?: number }) {
+	const url = "https://api.met.no/weatherapi/locationforecast/2.0/complete";
+
+	const params = new URLSearchParams();
+	params.append("lat", latitude.toString());
+	params.append("lon", longitude.toString());
+
+	if (altitude) {
+		params.append("altitude", altitude.toString());
+	}
+
+	const fullUrl = `${url}?${params.toString()}`;
+
+	try {
+		const response = await fetch(fullUrl, {
+			headers: {
+				"User-Agent": "Personal web forecast application for fun.",
+				"Content-Type": "application/json;charset=UTF-8",
+			},
+		});
+
+		const json = await response.json();
+
+		if (response.ok) {
+			return json as MetJsonForecast;
+		}
+
+		return {
+			message: response.statusText,
+			status_code: response.status,
+		} satisfies ApiError;
+	} catch (error) {
+		const _error = error as Error;
+		return {
+			message: "fetch getForecastData failed.",
+			error: _error,
+		} satisfies ApiError;
+	}
+}
