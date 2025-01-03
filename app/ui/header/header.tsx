fcdaaf9eb3c0533e2fb5719a69d047615e3cf5e4
radekBednarik <radek.bednarik@stesena.com>
2025-01-03 09:49:52 +0100

    readded png icons and added sun icon to the header

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import weatherImage from "../../../public/weather-icons/clearsky_day.png";
 
 export default function Header() {
 	return (
-		<header id="header" className="flex justify-end h-screen">
-			<div className="inline-flex flex-col ">
+		<header id="header" className="flex justify-between items-center">
+			<Image
+				src={weatherImage}
+				alt="Stylized image of the big bright yellow sun."
+			/>
+			<div className="inline-flex flex-col">
 				<h1 className="text-8xl">Weather Forecast</h1>
 				<h2 className="text-5xl self-end">for your location</h2>
 			</div>
--- public/weather-icons/README.md
+++
@@ -1,4 +0,0 @@
-# License
-
-Obtained from [Metno github repo](https://github.com/metno/weathericons/)
-under MIT License.
