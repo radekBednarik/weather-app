d064907f563728b07bc4a18983a6c33990a227e5
radekBednarik <radek.bednarik@tesena.com>
2025-01-10 09:04:01 +0100

    pagination navigation part works

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,33 @@
 
 import { WeatherForecastContext } from "@/app/contexts/weather-data-context";
 import GeolocationInfobox from "@/app/ui/geolocation/geolocation-infobox";
-import { useContext } from "react";
-import WeatherForecastList from "./ui/weather/weather-forecast-list";
+import WeatherForecastList from "@/app/ui/weather/weather-forecast-list";
+import { use, useContext } from "react";
 
-export default function Home() {
-  const context = useContext(WeatherForecastContext);
-  // for now just throw
-  if (typeof context === "undefined")
-    throw new Error("Return context cannot be undefined.");
+const itemsPerPage = 4;
 
-  return (
-    <main className="flex flex-col mt-10">
-      <GeolocationInfobox />
-      <WeatherForecastList itemsIndexStart={0} itemsIndexEnd={8} />
-    </main>
-  );
+export default function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+	const context = useContext(WeatherForecastContext);
+	// for now just throw
+	if (typeof context === "undefined")
+		throw new Error("Return context cannot be undefined.");
+
+	const p = use(searchParams).page;
+	const page = Number.parseInt(p ? p : "1") - 1;
+	const startIndex = page === 0 ? page : itemsPerPage * page;
+	const endIndex = startIndex + itemsPerPage;
+
+	return (
+		<main className="flex flex-col mt-10">
+			<GeolocationInfobox />
+			<WeatherForecastList
+				itemsIndexStart={startIndex}
+				itemsIndexEnd={endIndex}
+			/>
+		</main>
+	);
 }
