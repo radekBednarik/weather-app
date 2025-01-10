0aa56a3515ef0198f4e71f116f8659d5b6a1cb0d
radekBednarik <radek.bednarik@tesena.com>
2025-01-10 09:33:40 +0100

    add pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@
 import { WeatherForecastContext } from "@/app/contexts/weather-data-context";
 import GeolocationInfobox from "@/app/ui/geolocation/geolocation-infobox";
 import WeatherForecastList from "@/app/ui/weather/weather-forecast-list";
-import { use, useContext } from "react";
+import Link from "next/link";
+import { type FC, use, useContext } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 const itemsPerPage = 4;
 
@@ -12,15 +14,14 @@ export default function Home({
 }: {
 	searchParams: Promise<{ [key: string]: string | undefined }>;
 }) {
-	const context = useContext(WeatherForecastContext);
-	// for now just throw
-	if (typeof context === "undefined")
-		throw new Error("Return context cannot be undefined.");
-
 	const p = use(searchParams).page;
-	const page = Number.parseInt(p ? p : "1") - 1;
-	const startIndex = page === 0 ? page : itemsPerPage * page;
+	const page = Number.parseInt(p ? p : "1");
+	const startIndex = itemsPerPage * (page - 1);
 	const endIndex = startIndex + itemsPerPage;
+
+	const context = useContext(WeatherForecastContext);
+
+	if (!context) throw new Error("Context cannot be undefined.");
 
 	return (
 		<main className="flex flex-col mt-10">
@@ -29,6 +30,28 @@ export default function Home({
 				itemsIndexStart={startIndex}
 				itemsIndexEnd={endIndex}
 			/>
+			<Pagination currentPage={page} />
 		</main>
 	);
 }
+
+interface PaginationProps {
+	currentPage: number;
+}
+
+const Pagination: FC<PaginationProps> = ({ currentPage }) => {
+	const prevPage = currentPage > 1 ? currentPage - 1 : 1;
+	const nextpage = currentPage + 1;
+
+	return (
+		<div id="pagination" className="flex flex-row items-center justify-around">
+			<Link href={`/?page=${prevPage}`}>
+				<FaArrowLeft className="md:text-5xl" />
+			</Link>
+			<div className="border rounded-lg  p-2 md:text-4xl">{currentPage}</div>
+			<Link href={`/?page=${nextpage}`}>
+				<FaArrowRight className="md:text-5xl" />
+			</Link>
+		</div>
+	);
+};
