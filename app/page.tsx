8c4da1f03aa4dc38e688cfbce7ff9831bf4e588e
radekBednarik <radek.bednarik@tesena.com>
2025-01-10 10:23:49 +0100

    first pass of pagination is done

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import WeatherForecastList from "@/app/ui/weather/weather-forecast-list";
 import Link from "next/link";
 import { type FC, use, useContext } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import type { MetJsonForecast } from "./lib/met-api/declarations";
 
-const itemsPerPage = 4;
+const itemsPerPage = 6;
 
 export default function Home({
 	searchParams,
@@ -30,26 +31,43 @@ export default function Home({
 				itemsIndexStart={startIndex}
 				itemsIndexEnd={endIndex}
 			/>
-			<Pagination currentPage={page} />
+			<Pagination
+				currentPage={page}
+				context={context}
+				itemsPerPage={itemsPerPage}
+			/>
 		</main>
 	);
 }
 
 interface PaginationProps {
+	context: MetJsonForecast;
 	currentPage: number;
+	itemsPerPage: number;
 }
 
-const Pagination: FC<PaginationProps> = ({ currentPage }) => {
+const Pagination: FC<PaginationProps> = ({
+	currentPage,
+	context,
+	itemsPerPage,
+}) => {
+	const maxPages = Math.ceil(
+		context.properties.timeseries.length / itemsPerPage,
+	);
 	const prevPage = currentPage > 1 ? currentPage - 1 : 1;
-	const nextpage = currentPage + 1;
+	const nextpage = currentPage < maxPages ? currentPage + 1 : currentPage;
 
 	return (
-		<div id="pagination" className="flex flex-row items-center justify-around">
-			<Link href={`/?page=${prevPage}`}>
+		<div
+			id="pagination"
+			className="flex flex-row items-center justify-around my-10"
+		>
+			<Link href={`/?page=${prevPage}`} className="hover:text-blue-500">
 				<FaArrowLeft className="md:text-5xl" />
 			</Link>
-			<div className="border rounded-lg  p-2 md:text-4xl">{currentPage}</div>
-			<Link href={`/?page=${nextpage}`}>
+
+			<div className="border rounded-lg p-2 md:text-4xl">{currentPage}</div>
+			<Link href={`/?page=${nextpage}`} className="hover:text-blue-500">
 				<FaArrowRight className="md:text-5xl" />
 			</Link>
 		</div>
