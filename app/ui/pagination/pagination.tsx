1fb33038a7e82c9b2468e8a3f1164966ab676ea6
radekBednarik <radek.bednarik@tesena.com>
2025-01-13 15:42:22 +0100

    more beautiful spinner

--- app/ui/pagination/pagination.tsx
+++ app/ui/pagination/pagination.tsx
@@ -4,45 +4,45 @@ import type { FC } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 interface PaginationProps {
-	context: MetJsonForecast;
-	currentPage: number;
-	itemsPerPage: number;
+  context: MetJsonForecast;
+  currentPage: number;
+  itemsPerPage: number;
 }
 
 const Pagination: FC<PaginationProps> = ({
-	currentPage,
-	context,
-	itemsPerPage,
+  currentPage,
+  context,
+  itemsPerPage,
 }) => {
-	const maxPages = Math.ceil(
-		context.properties.timeseries.length / itemsPerPage,
-	);
-	const prevPage = currentPage > 1 ? currentPage - 1 : 1;
-	const nextpage = currentPage < maxPages ? currentPage + 1 : currentPage;
+  const maxPages = Math.ceil(
+    context.properties.timeseries.length / itemsPerPage,
+  );
+  const prevPage = currentPage > 1 ? currentPage - 1 : 1;
+  const nextpage = currentPage < maxPages ? currentPage + 1 : currentPage;
 
-	return (
-		<div
-			id="pagination"
-			className="flex flex-row items-center justify-around my-10"
-		>
-			<Link
-				href={`/?page=${prevPage}`}
-				className="hover:text-blue-500"
-				aria-label={`Navigate to ${prevPage} page.`}
-			>
-				<FaArrowLeft className="md:text-5xl" />
-			</Link>
+  return (
+    <div
+      id="pagination"
+      className="flex flex-row items-center justify-around my-10"
+    >
+      <Link
+        href={`/?page=${prevPage}`}
+        className="hover:text-blue-500"
+        aria-label={`Navigate to ${prevPage} page.`}
+      >
+        <FaArrowLeft className="md:text-3xl" />
+      </Link>
 
-			<div className="border rounded-lg p-2 md:text-4xl">{currentPage}</div>
-			<Link
-				href={`/?page=${nextpage}`}
-				className="hover:text-blue-500"
-				aria-label={`Navigate to ${nextpage} page.`}
-			>
-				<FaArrowRight className="md:text-5xl" />
-			</Link>
-		</div>
-	);
+      <div className="border rounded-lg p-2 md:text-3xl">{currentPage}</div>
+      <Link
+        href={`/?page=${nextpage}`}
+        className="hover:text-blue-500"
+        aria-label={`Navigate to ${nextpage} page.`}
+      >
+        <FaArrowRight className="md:text-3xl" />
+      </Link>
+    </div>
+  );
 };
 
 export default Pagination;
