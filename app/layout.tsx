ee898e06c2d35c1ffa04e7323a1322e42f276ab1
radekBednarik <radek.bednarik@tesena.com>
2025-01-17 08:54:01 +0100

    add footer and readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import WeatherForecastProvider from "@/app/contexts/weather-data-context";
+import { roboto } from "@/app/ui/fonts/fonts";
+import Footer from "@/app/ui/footer/footer";
 import Header from "@/app/ui/header/header";
 import HeaderForecastSummaryIcon from "@/app/ui/images/header-forecast-summary-image";
-import { roboto } from "@/app/ui/fonts/fonts";
 
 export const metadata: Metadata = {
-  title: "Weather forecast",
-  description: "Get weather forecast for your current location.",
+	title: "Weather forecast",
+	description: "Get weather forecast for your current location.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${roboto.className} antialiased container mx-auto my-auto mt-4 relative`}
-      >
-        <Header />
-        <WeatherForecastProvider>
-          <HeaderForecastSummaryIcon />
-          {children}
-        </WeatherForecastProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${roboto.className} antialiased container mx-auto my-auto mt-4 relative`}
+			>
+				<Header />
+				<WeatherForecastProvider>
+					<HeaderForecastSummaryIcon />
+					{children}
+				</WeatherForecastProvider>
+				<Footer />
+			</body>
+		</html>
+	);
 }
