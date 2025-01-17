0c008b024e656db58a489fd4dc3c7189b3e9b674
radekBednarik <radek.bednarik@tesena.com>
2025-01-17 08:54:13 +0100

    add footer and readme

---
+++ app/ui/footer/footer.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import ghLogo from "../../../public/logos/github-mark-white.png";
+
+const Footer = () => {
+	return (
+		<footer
+			id="footer"
+			className="flex flex-row items-center justify-end gap-4 my-4"
+		>
+			<div id="met-api">
+				Data provided by{" "}
+				<Link
+					href="https://api.met.no/weatherapi/locationforecast/2.0/documentation"
+					target="_blank"
+					aria-label="External link to MET Weather API website."
+					className="underline"
+				>
+					MET Weather API
+				</Link>
+			</div>
+			<div className="mx-2">|</div>
+			<div id="github-repo-link">
+				<Link
+					href="https://github.com/radekBednarik/weather-app"
+					aria-label="External link to this site code repository."
+					target="_blank"
+				>
+					<Image src={ghLogo} height={20} width={20} alt="Github logo." />
+				</Link>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
