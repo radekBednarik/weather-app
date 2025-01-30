300fc87ad47eacca34578ab2414361d4f9876bc0
radekBednarik <radek.bednarik@tesena.com>
2025-01-30 12:23:25 +0100

    create tables and upsert calls added

--- apps/harvester/src/db/postgres.ts
+++ apps/harvester/src/db/postgres.ts
@@ -1,1 +1,45 @@
+import type {
+	ForecastTimeStep,
+	PointGeometry,
+} from "@/met-api/declarations.js";
 import { sql } from "@vercel/postgres";
+
+async function upsertDataCoordinates(data: PointGeometry) {
+	await sql`
+    INSERT INTO coordinates (id, data)
+    VALUES (1, ${JSON.stringify(data.coordinates)})
+    ON CONFLICT (id)
+    DO UPDATE SET data = ${JSON.stringify(data.coordinates)}
+  `;
+}
+
+async function upsertDataTimeseries(data: ForecastTimeStep[]) {
+	await sql`
+    INSERT INTO timeseries (id, data)
+    VALUES (1, ${JSON.stringify(data)})
+    ON CONFLICT (id)
+    DO UPDATE SET data = ${JSON.stringify(data)}
+  `;
+}
+
+export async function upsertDataTables(
+	coordinates: PointGeometry,
+	forecastTimeSteps: ForecastTimeStep[],
+) {
+	await Promise.all([
+		upsertDataCoordinates(coordinates),
+		upsertDataTimeseries(forecastTimeSteps),
+	]);
+}
+
+export async function createTables() {
+	await sql`CREATE TABLE IF NOT EXISTS coordinates (
+      id SERIAL PRIMARY KEY,
+      data JSON
+    );`;
+
+	await sql`CREATE TABLE IF NOT EXISTS timeseries (
+      id SERIAL PRIMARY KEY,
+      data JSON
+    );`;
+}
--- apps/harvester/src/index.ts
+++ apps/harvester/src/index.ts
@@ -1,3 +1,4 @@
+import { createTables, upsertDataTables } from "@/db/postgres.js";
 import { getForecastData } from "@/met-api/api.js";
 import "dotenv/config";
 
@@ -7,8 +8,15 @@ async function main() {
 		longitude: 15.1191552,
 		altitude: null,
 	});
+	const pointGeometry = forecastData.data?.geometry;
+	const timeseries = forecastData.data?.properties.timeseries;
 
-	console.log(JSON.stringify(forecastData));
+	if (!pointGeometry || !timeseries) {
+		throw new Error("Failed to get needed data from MET API.");
+	}
+
+	await createTables();
+	await upsertDataTables(pointGeometry, timeseries);
 }
 
 (async () => {
